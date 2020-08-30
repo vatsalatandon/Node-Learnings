56b5381ef882e547b1245c29fa2a6021929ba917
vatsala.tandon <vatsala.tandon@CYG391>
2020-08-30 17:09:03 +0530

    Change Color

--- NotesApp/app.js
+++ NotesApp/app.js
@@ -4,4 +4,4 @@ const notes = require('./notes.js')
 const getnotes = notes()
 console.log(getnotes)
 
-console.log(chalk.bold.underline.green.inverse('Success'))
+console.log(chalk.bold.underline.Yellow('Success'))
