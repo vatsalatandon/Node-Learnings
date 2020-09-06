891b8449f69e3b7e2c9b8a703997f18605c16adc
vatsala.tandon <vatsala.tandon@CYG391>
2020-09-06 16:56:41 +0530

    Adding yargs to parse arguments

--- NotesApp/app.js
+++ NotesApp/app.js
@@ -1,12 +1,45 @@
 const chalk = require('chalk')
+const yargs = require('yargs')
 const notes = require('./notes.js')
+const { command } = require('yargs')
 
-const command = process.argv[2]
-console.log(process.argv)
+//customise yargs version
+yargs.version('1.1.0')
 
-if (command === 'add'){
-    console.log('Adding Note!')
-}
-else if (command === 'remove'){
-    console.log('Removing Note')
-}
+//add,remove,read,list
+//Create add command
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    handler: function(){
+        console.log('Adding a new note!')
+    }
+})
+
+//Create remove command
+yargs.command({
+    command: 'remove',
+    description:'Remove a note',
+    handler: function(){
+        console.log('Removing the note')
+    }
+})
+
+//Create list command
+yargs.command({
+    command: 'list',
+    description: 'List the notes',
+    handler: function(){
+        console.log('List of all notes')
+    }
+})
+//Create read command
+yargs.command({
+    command: 'read',
+    description: 'Reading the notes',
+    handler: function(){
+        console.log('Reading a note')
+    }
+})
+
+console.log(yargs.argv)
