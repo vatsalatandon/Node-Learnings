6d378ed62bb765d8a57dfe04a2f4e1215b21aee0
vatsala.tandon <vatsala.tandon@CYG391>
2020-09-28 11:26:52 +0530

    Adding a Note (Json)

--- NotesApp/app.js
+++ NotesApp/app.js
@@ -24,8 +24,7 @@ yargs.command({
         }
     },
     handler: function(argv){
-        console.log('Title:' + argv.title)
-        console.log('Body:' + argv.body)
+        notes.addNotes(argv.title,argv.body);
     }
 })
 
--- NotesApp/notes.js
+++ NotesApp/notes.js
@@ -1,2 +1,42 @@
+const fs = require('fs')
 const getNotes = function (){return "Your Notes are ready"};
-module.exports = getNotes
+
+const addNote = function(title,body){
+    const notes = loadNotes();
+    const duplicateNotes = notes.filter(function (note){
+        return note.title === title
+    })
+
+    if(duplicateNotes.length === 0){
+        notes.push({
+            title: title,
+            body: body
+        })
+        saveNotes(notes)
+        console.log('New note added!')
+    }
+    else {
+        console.log('Note tite taken!')
+    }
+}
+
+const loadNotes = function (){
+    try {
+        const dataBuffer = fs.readFileSync('notes.json')
+        const dataJson = dataBuffer.toString()
+        return JSON.parse(dataJson)
+    }
+    catch (e) {
+        return []
+    }
+}
+
+const saveNotes = function (notes) {
+    const dataJson = JSON.stringify(notes)
+    fs.writeFileSync('notes.json',dataJson)
+}
+
+module.exports = {
+    getNotes: getNotes,
+    addNotes: addNote
+}
