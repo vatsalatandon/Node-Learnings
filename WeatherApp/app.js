c79d9fbdd039b69b240ea5c2fa17dd8f50b512ed
vatsala.tandon <vatsala.tandon@CYG391>
2020-10-17 16:35:04 +0530

    Making Http Request

--- WeatherApp/app.js
+++ WeatherApp/app.js
@@ -1,11 +1,8 @@
-console.log('Starting')
+const request = require('request')
 
-setTimeout(() => {
-    console.log('2 second timer')
-},2000)
+const url ='http://api.weatherstack.com/current?access_key=274ea563cf13ed844c1bccd56a6eb2b7&query=37.8267,-122.4233'
 
-setTimeout(() => {
-    console.log('0 second timer')
-},0)
-
-console.log('Stopping')
+request({url: url},(error,response) => {
+    const data = JSON.parse(response.body)
+    console.log(data.current)
+})
