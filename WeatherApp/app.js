137a8a8a6fd673c960bc701a5a4bb761d5afbf75
vatsala.tandon <vatsala.tandon@CYG391>
2020-10-17 17:29:52 +0530

    Customising API Regests

--- WeatherApp/app.js
+++ WeatherApp/app.js
@@ -1,8 +1,10 @@
 const request = require('request')
 
-const url ='http://api.weatherstack.com/current?access_key=274ea563cf13ed844c1bccd56a6eb2b7&query=37.8267,-122.4233'
+//const url ='http://api.weatherstack.com/current?access_key=274ea563cf13ed844c1bccd56a6eb2b7&query=37.8267,-122.4233'
+const url ='http://api.weatherstack.com/current?access_key=274ea563cf13ed844c1bccd56a6eb2b7&query=37.8267,-122.4233&units=f'
 
-request({url: url},(error,response) => {
-    const data = JSON.parse(response.body)
-    console.log(data.current)
+request({url: url, json: true},(error,response) => {
+    //console.log(response.body.current)
+    console.log(response.body.current.weather_descriptions[0] + '. It is currently '+ response.body.current.temperature + ' degree out. It feels like ' + response.body.current.feelslike +' degree')
+    // Change Unit
 })
